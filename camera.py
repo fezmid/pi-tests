f6b83703867c104bcf86c6189754926b5184e854
Christopher White <fezmid@gmail.com>
2020-01-27 20:50:10 -0600

    Version 0.2: Added comments in camera.py

--- camera.py
+++ camera.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 
+# Import libraries
 from picamera import PiCamera
 from time import sleep
 
+# Define the object
 camera = PiCamera()
 
+# Turn on the camera preview, sleep 10 seconds, then turn off the camera
 camera.start_preview()
 sleep(10)
 camera.stop_preview()
